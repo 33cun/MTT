a670000663134ce540f679525d4d74bf0c117954
Eleven <xiaoxin@evrazlabs.email>
2020-08-25 16:17:27 +0800

    0.0.1

---
+++ Example/MTT/MTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MTAppDelegate.h
+//  MTT
+//
+//  Created by 33cun on 08/25/2020.
+//  Copyright (c) 2020 33cun. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
